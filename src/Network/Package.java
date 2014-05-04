df53d3f3d55d4f1661a45fd22479f089f20b7044
Jonatan Karlsson <me@jonatankarlsson.se>
2014-05-04 19:34:40 +0200

    Added a toString

--- src/Network/Package.java
+++ src/Network/Package.java
@@ -1,23 +1,24 @@
 package Network;
 
+import Game.Player;
 import java.io.Serializable;
 
 public class Package implements Serializable {
 
 	private static final long serialVersionUID = -1494999107286302383L;
 
-	private String name;
+	private Player player;
 	private int row;
 	private int col;
 
 	Package(int row, int col) {
-		this(row, col, "Unknown");
+		this(row, col, new Player());
 	}
 
-	Package(int row, int col, String name) {
+	Package(int row, int col, Player player) {
 		this.row = row;
 		this.col = col;
-		this.name = name;
+		this.player = player;
 	}
 
 	public int getRow() {
@@ -36,11 +37,18 @@ public class Package implements Serializable {
 		this.col = col;
 	}
 
-	public String getName() {
-		return name;
+	public Player getPlayer() {
+		return player;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setPlayer(Player player) {
+		this.player = player;
 	}
+
+	@Override
+	public String toString() {
+		return "Package [player=" + player + ", row=" + row + ", col=" + col
+				+ "]";
+	}
+
 }
